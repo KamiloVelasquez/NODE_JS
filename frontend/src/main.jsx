114ddc05b14e70490123a895b4a858ec925a3d81
KamiloVelasquez <camilo.velasquez@est.iudigital.edu.co>
2026-03-24 23:59:54 -0500

    configugacion vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
 import { LanguageProvider } from './contexts/language-context.jsx'
 
 createRoot(document.getElementById('root')).render(
